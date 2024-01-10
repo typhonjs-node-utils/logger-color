d4a608748d1d485e01ed3ad2f702b557aafe1950
typhonrt <support@typhonjs.io>
2024-01-10 13:04:35 -0800

    Moved plugin implementation.

---
+++ src/plugin/index.js
@@ -0,0 +1,35 @@
+/**
+ * `@typhonjs-utils/logger-color/plugin` defines an instance of {@link ColorLogger} that can be loaded as a plugin via
+ * `@typhonjs-plugin/manager` and self-registers on an eventbus with all methods exposed as event bindings.
+ *
+ * The extended API when using event bindings is accessed by appending `:compact`, `:nocolor`, `:raw`, or `:time`. The
+ * no color option outputting the message with the current log format and the raw format will output just the raw
+ * message with no format or color applied.
+ *
+ * @example
+ * import { PluginManager } from '@typhonjs-plugin/manager';
+ *
+ * const pluginManager = new PluginManager();
+ *
+ * // This will automatically wire up typhonjs-color-logger to the eventbus.
+ * await pluginManager.add({ name: '@typhonjs-utils/logger-color/plugin' });
+ *
+ * const eventbus = pluginManager.getEventbus();
+ *
+ * // simple usage
+ * eventbus.trigger('log:error', 'An error occurred!');
+ *
+ * @see https://www.npmjs.com/package/@typhonjs-plugin/manager
+ *
+ * @module
+ */
+import { ColorLogger } from '@typhonjs-utils/logger-color';
+
+/**
+ * A default exported instance of {@link ColorLogger}.
+ *
+ * @type {import('@typhonjs-utils/logger-color').ColorLogger}
+ */
+const logger = new ColorLogger();
+
+export default logger;
